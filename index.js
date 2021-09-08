bf20e7652c7810c29efc967627f14c9e2d85454f
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-09-07 21:54:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+findMatching = (drivers,name) => {
+  return drivers.filter( x => x.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter( x => x.slice(0, string.length) === string);
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(x => x.name === string); 
+}
